66d44d1917e2f5a3853a5ef9659cc0b1df98a706
Taaran <taaran06@gmail.com>
2026-07-25 18:20:27 +0530

    task 3 - Task Manager

--- TaskManager/backend/models/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const DB_URL = process.env.DB_URL
-mongoose.connect(DB_URL).then(()=>{
-    console.log("Mongoose is connected...");
-}).catch((err)=>{
-    console.log("MongoDB connection error: ", err);
-
-})
